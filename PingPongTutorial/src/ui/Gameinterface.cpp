b40b1cda445cb71efb4dbb73dba7c7bd22cb7436
KangYu Cheng <1119109855@qq.com>
2024-01-13 21:56:40 +0800

    feat: add interface

---
+++ PingPongTutorial/src/ui/Gameinterface.cpp
@@ -0,0 +1,15 @@
+#include "../../inc/ui/Interface.h"
+
+GameInterface::GameInterface() : Interface(L"Game") {}
+GameInterface::~GameInterface() {}
+
+void GameInterface::OnEnter() {}
+
+void GameInterface::OnExit() {}
+
+void GameInterface::Update() {}
+
+void GameInterface::Draw()
+{
+	circle(500, 280, 100);
+}
